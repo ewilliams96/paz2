515e026135c9b08fd8b6973bdb70b198f595215c
Elizabeth Williams <elwilliams@cs.stonybrook.edu>
2016-04-16 18:48:58 -0400

    battle_mode progress?

--- battle_mode.py
+++ battle_mode.py
@@ -1,33 +1,87 @@
 import pyglet
 from pyglet.window import key
 
-
-window = pyglet.window.Window()
-image = pyglet.resource.image('images/seal.png')
-image.width = 200
-image.height = 200
-imageX = 640 // 2 - image.width // 2 
-imageY = 70 - image.height // 2
+# for testing
+a = animal.Animal(0, 0, 'seal', 10, 10)
+# sprite pos
+x_pos = 320
+y_pos = 240
 
 
 
-@window.event
-def on_draw():
+class BattleMode:
 
-	window.clear()
-	image.blit(imageX, imageY)
-	print("test")
+
+	attackMessage = "attack"
+	runMessage = "run"
+	def center_image(image):
+		image.anchor_x = image.width/2
+		image.anchor_y = image.height/2
+
+	def __init__(self):
+		self.image = None
+		self.player = None
+		self.battleMessage = None
+		self.level = None
+
+	def startBattle(animal, player=None, level=None):
+		
+		
+		imageName = 'images/' + animal.name + ".png"
+		self.image = pyglet.resource.image(imageName)
+		
+		self.image.width = 200
+		self.image.height = 200
+		center_image(image)
+		
+		
+		self.battleMessage = "You meet a " + a.name + " that has a mass of " + str(a.muscle + a.fat) +  "kg."
+		
+
+	def battleModeDraw():
+		image.blit()
+		display_label(battleMessage)
+
+	def attack():
+		Display.
+
+
+	def run():
+		Displa. 
+
+
+
+	
+
+
+
+def display_label(string, x=None, y=None):
+	label = pyglet.text.Label(string,
+                          font_name='Courier',
+                          font_size=18, 
+                          x=320, y=100,
+                          anchor_x='center', anchor_y='center', width=600, multiline=True)
+	label.draw()
+
+# draw battle screen
+#@window.event
+#def on_draw():
+	# draw animal
+#	window.clear()
+	#image.blit(x_pos, y_pos)
+#	print("test")
+	# draw rectangle for text box
+	#pyglet.graphics.draw()
+#	display_label(battleMessage, 320, 100)
+
+
+	
+
+
 
 
 # advance text
 # choose 
-@window.event
-def on_key_press(symbol, modifiers):
-	global imageX
-	global imageY
-	imageX = imageX + 32
-	imageY = imageY + 32
-	on_draw()
 
 
 
